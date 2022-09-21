74e47037f68195ad41789aae67d9f8e776e7eada
eliasollila <elias.ollila@syk.fi>
2022-09-21 15:19:23 +0300

    nyt

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for(int i=0; i<=10; i++)
+        {
+            double k=i*(Math.PI/5);
+            System.out.println(k + ", " + Math.sin(k) + ", " + Math.cos(k));
+        }
     }
+
 }
